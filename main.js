3c353d59be31f8c46504f035a03ded93b296132c
Tolikbu <tolikelement@gmail.com>
2021-11-26 14:25:30 +0500

    feat(main.js): new lesson02

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "lesson02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 165132;
+let rollback = 15;
+let fullPrice = 3158620;
+let adaptive = true;
 
-console.log("Hello Tolik");
-
-alert("закройте данное окно");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимсоть верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимсоть разработки сайта" + " " + fullPrice + " " + "рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100) + " " + "рублей");
