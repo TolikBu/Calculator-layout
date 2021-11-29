c76626be459014783fc57379c5f36fff4ee38faf
TolikBu <tolikelement@gmail.com>
2021-11-29 22:59:14 +0500

    fix main.js: all fix

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 'use strict';
 
-let title = prompt("Как называется ваш проект?");
-console.log(title);
-let screens = prompt("Какие типы экранов нужно разработать?", 'пример:"Простые, Сложные, Интерактивные"');
-console.log(screens);
-let screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
-console.log(parseFloat(screenPrice));
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(!!adaptive);
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service1);
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-console.log(parseFloat(servicePrice1));
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service2);
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-console.log(parseFloat(servicePrice2));
-
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", 'пример:"Простые, Сложные, Интерактивные"');
+const screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
 
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
 let servicePercentPrice = fullPrice - (fullPrice * 0.15);
+
+console.log(title);
+console.log(screens);
+console.log(parseFloat(screenPrice));
+console.log(!!adaptive);
+console.log(service1);
+console.log(parseFloat(servicePrice1));
+console.log(service2);
+console.log(parseFloat(servicePrice2));
+
+console.log(fullPrice);
 console.log(Math.ceil(servicePercentPrice));
 
 switch (true) {
